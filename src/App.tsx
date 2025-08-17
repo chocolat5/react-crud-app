4543954287bbf1422161df761fecfe854e1e3050
chocolat5 <futatsugi.y@gmail.com>
2025-08-17 11:24:17 +0300

    chore: delete unnecessary data/code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import styled from "@emotion/styled";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import "./App.css";
 
 const StyledLogo = styled.img`
   height: 6em;
